de6c94e7f4aa3e856293084d54be08f6e49530ab
GiorgosCut <giorgos.arsenal@gmail.com>
2024-10-26 11:16:20 +0300

    Removed sample code, and created a Get request controller, returning two sample house objects

---
+++ MagicHouse_API/Controllers/HouseAPIController.cs
@@ -0,0 +1,21 @@
+﻿using MagicHouse_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicHouse_API.Controllers
+{
+
+    [Route("api/HouseAPI")]
+    [ApiController]
+    public class HouseAPIController : ControllerBase
+    {   
+        [HttpGet]
+        public IEnumerable<House> GetHouses()
+        {
+            return new List<House>()
+            {
+                new House { Id = 1, Name = "House with garden" },
+                new House { Id = 2, Name = "House with pool" }
+            };
+        }
+    }   
+}
