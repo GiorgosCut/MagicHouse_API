70375206dbfe5b2c28f78d70c7914e6293f86de0
GiorgosCut <giorgos.arsenal@gmail.com>
2024-11-24 15:58:48 +0200

    Added Api endpoint for deleting a house

--- MagicHouse_API/Controllers/HouseAPIController.cs
+++ MagicHouse_API/Controllers/HouseAPIController.cs
@@ -41,7 +41,7 @@ namespace MagicHouse_API.Controllers
         public ActionResult<HouseDTO> CreateHouse([FromBody] HouseDTO house)
         {
             //Check if house name already exists
-            if(HouseStore.houses.FirstOrDefault(x => x.Name.ToLower() == house.Name.ToLower()) != null)
+            if (HouseStore.houses.FirstOrDefault(x => x.Name.ToLower() == house.Name.ToLower()) != null)
             {
                 //Add an error to the ModelState, with a custom name and message
                 ModelState.AddModelError("DuplicateNameError", "Villa name is not unique!");
@@ -55,11 +55,33 @@ namespace MagicHouse_API.Controllers
             }
             house.Id = HouseStore.houses.OrderByDescending(x => x.Id).First().Id + 1;
             HouseStore.houses.Add(house);
-            
+
             //Instead of returning status 200 OK, we return 201 created, while also returning the API location where the object which was created can be found.
             //Here you call the GetHouse controller, passing the id of the house you just created.
             //so it will also return "api/HouseAPI/the_id_of_the_new_object"
-            return CreatedAtRoute("GetHouse", new {id = house.Id}, house);
+            return CreatedAtRoute("GetHouse", new { id = house.Id }, house);
+        }
+
+        [HttpDelete("{id:int}", Name = "DeleteVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult DeleteHouse(int id) {
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+            var house = HouseStore.houses.FirstOrDefault(x => x.Id == id);
+            if (house is not null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                HouseStore.houses.Remove(house);
+            }
+            
+            return NoContent();
         }
     }  
 }
